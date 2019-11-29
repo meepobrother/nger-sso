3ae589ad317080897420d13c5827dbc9e9c0bfa5
imeepos <1037483576@qq.com>
2019-11-29 19:21:09 +0800

    finish hello word

--- packages/api/lib/controllers/verifySsoToken.controller.ts
+++ packages/api/lib/controllers/verifySsoToken.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from "@nestjs/common";
 
 @Controller()
 export class VerifySsoTokenController {
-    @Get()
+    @Get(`verifySsoToken`)
     verifySsoToken() { 
         return `verify sso token`
     }
